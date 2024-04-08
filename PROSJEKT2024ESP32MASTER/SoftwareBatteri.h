2d2c606584143348c96c6e7192b9a1542cec5890
stangeqwq <StangelandEricJoshua@gmail.com>
2024-04-08 14:50:10 +0200

    battery and speed module

---
+++ PROSJEKT2024ESP32MASTER/SoftwareBatteri.h
@@ -0,0 +1,11 @@
+// SoftwareBatteri.h
+
+#ifndef SOFTWARE_BATTERI
+#define SOFTWARE_BATTERI
+
+// Function declarations
+float UpdateVariables();
+void BatteryStatusDisplay();
+void SpeedoMeter();
+
+#endif
