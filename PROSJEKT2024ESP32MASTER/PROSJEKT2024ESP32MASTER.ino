76ee395d98f6e624eb99f7324ef1dd533d792df2
ArtoRontti <144996879+ArtoRontti@users.noreply.github.com>
2024-06-05 20:12:46 +0200

    Update PROSJEKT2024ESP32MASTER.ino
    added comments

--- PROSJEKT2024ESP32MASTER/PROSJEKT2024ESP32MASTER.ino
+++ PROSJEKT2024ESP32MASTER/PROSJEKT2024ESP32MASTER.ino
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include "UbidotsEsp32Mqtt.h"
+#include <SparkFun_APDS9960.h>
 
 #define I2C_SDA 13  //bus
 #define I2C_SCL 12  //bus
@@ -15,18 +16,33 @@ WebServer server(80);           // basic server
 
 //ubidots
 const char* UBIDOTS_TOKEN = "BBUS-AcTJ6ccXQVpNyEBYHJ1dRor0GteJmb";
-const char* DEVICE_LABEL = "esp32";
-const char* VARIABLE_LABEL = "Akselerasjon";
-const char* VARIABLE_LABELD = "discount";
-const char* VARIABLE_LABELE = "strompris";
+const char* DEVICE_LABEL = "zumoesp";
+const char* VARIABLE_LABELa = "akselerasjon";
+const char* VARIABLE_LABELd = "discount";
+const char* VARIABLE_LABELs = "strompris";
+const char* VARIABLE_LABELr = "redZone";
+const char* VARIABLE_LABELg = "greenZone";
+const char* VARIABLE_LABELb = "blueZone";
 Ubidots ubidots(UBIDOTS_TOKEN);
+
+//RGBsensor
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+int redZone, greenZone, blueZone = 0;
+unsigned long previousMillisRGBsensor = 0;
+uint16_t ambient_light, red_light, green_light, blue_light = 0;
 
 //discount
 float discount;
 
+//clock
+float hour = 0.0;
+
+//acceleration
+float acceleration = 0;
+
 //I2C
 float newBattery = 0;
-float newSpeed = 0;
+float newAcceleration = 0;
 float newDiscount = 0;
 float receivedBattery;
 float receivedDiscount;
@@ -61,7 +77,8 @@ String generateBatteryHTML(float receivedBattery) {
 //////////////esp now///////////////////
 
 
-uint8_t broadcastAddress[] = { 0x44, 0x17, 0x93, 0x5e, 0x45, 0x0c };  // addresse til motpart
+uint8_t broadcastAddress[] = { 0x44, 0x17, 0x93, 0x5e, 0x45, 0x0c };  // addresse til joshua sin
+//uint8_t broadcastAddress[] = { 0x44, 0x17, 0x93, 0x5e, 0x48, 0x34 };  // addresse til eivind sin
 
 // Variable to store if sending data was successful
 String success;
@@ -76,6 +93,7 @@ struct_message incomingData;
 struct_message outgoingData;
 bool car2car = false;
 bool FromCar2Car = false;
+const unsigned long chargeInterval = 5000;
 
 char incomingMsg1[100];  //100 byte message
 char incomingMsg2[100];
@@ -107,10 +125,12 @@ void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
   Serial.println(receivedData->msg2);
 }
 
-// esp now FINITO//////////////////
+
 unsigned long espNowMillis;
 
 esp_now_peer_info_t peerInfo;
+// esp now FINITO//////////////////
+
 void setup() {
   //esp now
   WiFi.mode(WIFI_STA);
@@ -144,15 +164,17 @@ void setup() {
   Wire.begin();
   Serial.begin(115200);
 
-
   //ubidots
   ubidots.connectToWifi(ssid, password);
   ubidots.setCallback(callback);
   ubidots.setup();
   ubidots.reconnect();
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABEL);
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELD);
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELE);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELa);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELd);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELs);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELr);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELg);
+  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELb);
 
 
   // Connect to Wi-Fi
@@ -215,7 +237,8 @@ void setup() {
     content += "<div id='keyN' class='key' style ='grid-column: 6; grid-row: 26;'>regular</div>";  // N button for regular acceleration/ regular mode
     //charging modes
     content += "<div id='key1' class='key' style ='grid-column: 15; grid-row: 22;'>Charge</div>";   //1 for regular charge
-    content += "<div id='key2' class='key' style ='grid-column: 15; grid-row: 24;'>Car2Car</div>";  //2 for car to car charge
+    content += "<div id='key2' class='key' style ='grid-column: 15; grid-row: 24;'>FromCar</div>";  //2 for car to car charge
+    content += "<div id='key3' class='key' style ='grid-column: 17; grid-row: 26;'>ToCar</div>";  //3 for car to car charge
     //update battery level on website
     content += generateBatteryHTML(receivedBattery);  // to be able to update and show realtime battery level
     content += "</div>";
@@ -268,6 +291,8 @@ void setup() {
 
       else if (key == "2") {
         car2car = true;
+      } else if (key == "3") {
+        FromCar2Car = true;
       }
     } else {
       server.send(400, "text/plain", "Bad request");
@@ -276,6 +301,7 @@ void setup() {
 
   server.begin();
   Serial.println("HTTP server started");
+  rgbSensorSetup();
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -292,17 +318,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void loop() {
   server.handleClient();
-
   if (!ubidots.connected()) {
     ubidots.reconnect();
-    ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABEL);
-    ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELD);
-    ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABELE);
   }
   ubidots.loop();
 
+  clockTime();
   receiveData();
-
+  findAacceleration();
+  zoneCheck();
   carToCarReceive();
   CarToCarSend();
 }
@@ -311,79 +335,194 @@ void loop() {
 void receiveData() {
   Wire.requestFrom(8, sizeof(info));  // Request data from slave address 8
 
-  /*if (Wire.available() != sizeof(info)) {  //checks that the size matches
-    Serial.println("Received data size does not match struct size!");
-    return;
-  }*/
-
   Wire.readBytes((uint8_t*)&info, sizeof(sendInfo));  // reads the received info
 
   //access received data
-  float receivedSpeed = info.s;
   receivedDiscount = info.d;
   receivedBattery = info.p;
 
-  if (receivedSpeed != newSpeed) {
-    Serial.print("Speed: ");
-    Serial.println(receivedSpeed);
-    ubidots.add(VARIABLE_LABEL, receivedSpeed);
+  if (acceleration != newAcceleration) {
+    ubidots.add(VARIABLE_LABELa, acceleration);
     ubidots.publish();
-    newSpeed = receivedSpeed;
+    newAcceleration = acceleration;
   }
 
   if (receivedDiscount != newDiscount) {
     //Serial.print("Discount: ");
     //Serial.println(receivedDiscount);
-    /*yourElprice = elprice - receivedDiscount;
-    ubidots.add(VARIABLE_LABELD, yourElPrice);  //deducted el-price
-    ubidots.add(VARIABLE_LABELE, elPrice);      //general el-price
+    float yourElPrice = elPrice(hour) - (elPrice(hour) * receivedDiscount);
+    ubidots.add(VARIABLE_LABELd, yourElPrice);  //deducted el-price
+    ubidots.add(VARIABLE_LABELs, elPrice(hour));      //general el-price
     ubidots.publish();
-    newDiscount = receivedDiscount;*/
+    newDiscount = receivedDiscount;
   }
 
   if (receivedBattery != newBattery) {
     newBattery = receivedBattery;
   }
 }
+float elPrice(float hour) {//function for telling power price at differnt time of day
+  float price = 0.0;
+
+  if (hour >= 0 && hour < 6) {
+    // Low price at night
+    price = 25;
+  } else if (hour >= 6 && hour < 18) {
+    // high price in the day
+    price = 50;
+  } else {
+    // decent price in the evening
+    price = 30;
+  }
+  return price;
+}
+
+void clockTime(){//clock function for an alternativ time.
+  //millis variables
+  static unsigned long pastMillis = 0;
+  const unsigned long timeIntervalMillis = 10000; //10 seconds reprsent 1 "hour".
+    if (millis() - pastMillis >= timeIntervalMillis) {
+    pastMillis = millis();
+    hour += 1; // hour variable increas for every "hour"
+    if (hour > 24) {
+      hour = 0; // new day
+    }
+  }
+}
 
 void carToCarReceive() {
-
+  static unsigned long lastMillisR = 0;                          //charge timer
   if (car2car) {
-    FromCar2Car = false;
+    FromCar2Car = false;                                         //cant "send" battery whilst receiving
   }
 
-  if (strcmp(incomingMsg1, "Car2Car charging available") == 0) {
-    if (car2car) {
+  if ((strcmp(incomingMsg1, "car2car charge available") == 0)) { //if string equals Msg1
+    if (car2car) {                                               //if car2car is true, button 2 on website is pressed
       strcpy(outgoingData.msg1, "accepted car2car charge");
-      if (millis() % 5000 == 0) {  //5percent battery every 5 seconds
-        receivedBattery += 5.0;
-        if (receivedBattery >= 100){
-          strcpy(outgoingData.msg1, "car2car charge stopped");
-          car2car = false;  //stops charging when battery is full, or with stop command
+      espNowSend();                                              //sends accepted charge message
+      if (millis() - lastMillisR >= chargeInterval) {            //5percent battery every 5 seconds
+        lastMillisR = millis();
+        if (receivedBattery >= 94.0) {                           //if batterylevel is 94 or above
+          strcpy(outgoingData.msg1, "car2car charge stopped");   //sends a car2car stop message
+          espNowSend();
+          car2car = false;                                       //receive battery is set to false, charging is stopped
         }
-        else if(strcmp(incomingMsg1, "car2carCharge stopped")){
+        else if ((strcmp(incomingMsg1, "car2carCharge stopped") == 0)) {  //can also be stopped by incoming message from the other party
           car2car = false;
         }
+        Wire.beginTransmission(8);                               //slave handles the battery level change with wire communication
+        Wire.write('y');  
+        Wire.endTransmission();
       }
     }
   }
 }
 
 void CarToCarSend() {
+  static unsigned long lastMillisS = 0;                           //charge timer
 
-  if (FromCar2Car) {
-    car2car = false;
+  if (FromCar2Car) {                                              //cant "send" battery whilst receiving
+    car2car = false;                                              //sets receiving to false
   }
 
-  if ((receivedBattery >= 50 && !car2car) && FromCar2Car) {
-    strcpy(outgoingData.msg1, "car2car charge available");
-    if (strcmp(incomingMsg1, "accepted car2car charge") == 0) {
-      if (millis() % 5000 == 0) {
-        receivedBattery -= 5.0;
-        if ((receivedBattery < 50) || strcmp(incomingData.msg1, "car2car charge stopped")) {
-          FromCar2Car = false;
+  if ((receivedBattery >= 50.0) && FromCar2Car) {                 //can only "send" if battery level is above 50percent
+    strcpy(outgoingData.msg1, "car2car charge available");        //changes outgoing message
+    espNowSend();                                                 
+    if ((strcmp(incomingMsg1, "accepted car2car charge") == 0)) { //checks if chosen string matches received message 1
+      if (millis() - lastMillisS >= chargeInterval) {
+        lastMillisS = millis();
+        Wire.beginTransmission(8);
+        Wire.write('g');                                          //slave handles decreasing battery level with switch case case "g"
+        Wire.endTransmission();
+        if ((receivedBattery <= 50.0) || (strcmp(incomingData.msg1, "car2car charge stopped")==0)) { //can be stopped by battery level og incoming "stop" message
+          FromCar2Car = false;                                    //stops sending battery
+          strcpy(outgoingData.msg1, "car2car charge stopped");
+          espNowSend();                                           //sends stop message
         }
       }
     }
+  }
+}
+
+void espNowSend(){//procedure for sending message with espNow
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t*)&outgoingData, sizeof(outgoingData));
+      if (result == ESP_OK) {
+        Serial.println("Sent with success");
+      } else {
+        Serial.println("Error sending the data");
+      }
+}
+
+void rgbSensorSetup() {
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("APDS-9960 - ColorSensor"));
+  Serial.println(F("--------------------------------"));
+
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if (apds.init()) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+
+  // Start running the APDS-9960 light sensor (no interrupts)
+  if (apds.enableLightSensor(false)) {
+    Serial.println(F("Light sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during light sensor init!"));
+  }
+
+  // Wait for initialization and calibration to finish
+  delay(500);
+}
+
+void zoneCheck() {
+  //Millis variabels
+  static unsigned long lastUbiZone = 0;
+  const unsigned long ubiZoneInterval = 10000;
+  const int intervalRGBsensor = 100;
+  static char zone = 's';
+
+  if (millis() - previousMillisRGBsensor >= intervalRGBsensor) {
+    previousMillisRGBsensor = millis();
+    // Read the light levels (ambient, red, green, blue)
+    if (!apds.readAmbientLight(ambient_light) || !apds.readRedLight(red_light) || !apds.readGreenLight(green_light) || !apds.readBlueLight(blue_light)) {
+      Serial.println("Error reading light values");
+    }
+    if (ambient_light > 5000) {//check wich light is strongest
+      if (red_light > green_light + blue_light - 1000) {
+        zone = 'r';
+      } else if (green_light > red_light + blue_light - 2000) {
+        zone = 'g';
+      } else if (blue_light > red_light + green_light - 2000) {
+        zone = 'b';
+      }
+      Serial.println("You are in Zone: " + String(zone));
+    }
+  }
+  if (abs(acceleration) >= 2.25) {//If acceleration is to high, flagg the zone
+    if (zone == 'r') {
+      redZone += 1;
+    } else if (zone == 'g') {
+      greenZone += 1;
+    } else if (zone == 'b') {
+      blueZone += 1;
+    }
+    if(millis() - lastUbiZone >= ubiZoneInterval){//Send zones to ubidots every 10 sec
+      lastUbiZone = millis();
+      ubidots.add(VARIABLE_LABELr, redZone);
+      ubidots.add(VARIABLE_LABELg, greenZone);
+      ubidots.add(VARIABLE_LABELb, blueZone);
+      ubidots.publish(DEVICE_LABEL);
+    }
+  }
+}
+void findAacceleration() {//Meassuer acceleration based on speed
+  static float speedB, lastSpeedTime = 0;
+  if (millis() - lastSpeedTime >= 10) {
+    lastSpeedTime = millis();
+    acceleration = info.s - speedB;
+    speedB = info.s;
   }
 }
